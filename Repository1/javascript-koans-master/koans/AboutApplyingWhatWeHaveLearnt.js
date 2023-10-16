22de8e5551868a1241f059caff14b3cf2b68eed6
MariaNosova111 <mariykanosova1998@yandex.ru>
2023-10-16 20:03:38 +0700

    Done

--- Repository1/javascript-koans-master/koans/AboutApplyingWhatWeHaveLearnt.js
+++ Repository1/javascript-koans-master/koans/AboutApplyingWhatWeHaveLearnt.js
@@ -19,7 +19,7 @@ describe("About Applying What We Have Learnt", function() {
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
 
     var i,j,hasMushrooms, productsICanEat = [];
-
+    
     for (i = 0; i < products.length; i+=1) {
         if (products[i].containsNuts === false) {
             hasMushrooms = false;
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -41,7 +41,17 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+       var hasmushrooms = function(ingredient){
+		return ingredient !== "mushrooms";
+	  }
+
+	  _.filter(products, function(pizza){
+		if(pizza.containsNuts === false && _(pizza.ingredients).all(hasmushrooms)){
+				productsICanEat.push(pizza);
+			}			
+	  });
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +65,18 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
-    expect(233168).toBe(FILL_ME_IN);
+ /* try chaining range() and reduce() */
+    var numbers = _.range(0,1000)
+    let sum = numbers.reduce((sum,numbers) => {
+        if(numbers % 3 === 0 || numbers % 5 ===0){sum += numbers}
+        return sum;
+}); 
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,7 +89,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
@@ -83,7 +97,13 @@ describe("About Applying What We Have Learnt", function() {
 
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    _(products).chain().map(function(value){
+		return value.ingredients;
+	}).flatten().reduce(function(number, name){
+		return ingredientCount[name] = (ingredientCount[name] || 0) + 1;
+	},0);
+
+   expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
@@ -98,7 +118,7 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-
+    
 
   });
 
@@ -107,7 +127,7 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("should find the 10001st prime", function () {
-
+     var is_prime 
   });
   */
 });
